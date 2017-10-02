d7cc95c62f8da295c583cf875c30da93886db478
delanou <delanou@ou.edu>
2017-10-01 20:28:41 -0500

    File reogranization

--- js/script.js
+++ js/script.js
@@ -9,14 +9,8 @@
 
     // figure out how to get the value from chrome storage
     let defaultState = chrome.storage.local.get("storedState", function(result) {
-        alert("in chrome.get");
-        alert("result.value = " + result.value);
         defaultState = result.value;
-        alert("retrieving from local storage = " + defaultState)
     });
-
-
-    alert("The default state = " + defaultState);
 
     // only change webpage if state is not the one in memory
     if (!stateOptions.text().includes(defaultState)) {
@@ -26,17 +20,4 @@
             window.location.href = newOption.attr('href');
         });
     }
-
-    // // only run if allStates is not selected 
-    // if (!stateOptions.text().includes("All States"))
-    // {
-    //     let allOption = $("a.choice:contains('All States')");
-           
-    //     // when clicking set the window.location.href to the href in the tag
-    //     // use [0] to work with the dom element
-
-    //     allOption[0].click(function() {
-    //         window.location.href = allOption.attr('href');            
-    //     });
-    // }
 })();
